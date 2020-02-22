970809f118b8077c64f8af1560cdefaac3cb2348
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-02-22 17:59:16 +0100

    switched from sql to pdo format

--- templates/get-events.php
+++ templates/get-events.php
@@ -1,8 +1,18 @@
-<? require('../db/config.php'); 
+<? require('db/config.php');
+
+$sql = "SELECT * FROM events";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 //Pointing to what data to fetch/get
-$sql = "SELECT * FROM events";
+/*$sql = "SELECT * FROM events";
 
 $events = $conn->query($sql);
 
 $conn->close();
+
+?>
+*/

f8cb1e1391f41a3ba2b75bf4719717d5bdb7e712
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-02-22 20:40:07 +0100

    added bootstrap and style on events

--- templates/events.php
+++ templates/events.php
@@ -1,10 +1,19 @@
 <? require('get-events.php');?>
 
-
+<div class="row">
 <?
 //Looping each row in table as its own "article"
  foreach ($events as $event){ ?>
-    <? echo $event['title']?>
-    <? echo $event['text']?>
-    <? echo $event['img_url']?>
- <? }
+ <div class="col-lg-3">
+    <div class="card" style="width: 18rem;">
+  <img src="<? echo $event['img_url'] ?>" class="card-img-top" alt="picture">
+  <div class="card-body">
+    <h5 class="card-title" style="color: #3D4849;"><? echo $event['title'] ?></h5>
+    <p class="card-text"><? echo $event['text'] ?></p>
+    <a href="#" class="btn btn-primary" style="background-color: #B3997F;">Edit</a>
+    <a href="#" class="btn btn-secondary" style="width: 30% !important; height: 30% !imporant; background-color: #6DA0B3;">Delete</a>
+  </div>
+ </div>
+</div>
+ <? } ?>
+ </div>

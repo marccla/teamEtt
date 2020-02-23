518a032af602d788af3fbdee0290a4b57102e01e
marccla <marcclarsson@gmail.com>
2020-02-23 12:29:31 +0100

    added welcome.php with script for if logged in, fixed landing pages in login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <? 
-    include('db/config.php');
+    
     include('templates/head.php');
     include('templates/nav.php');
 
@@ -18,7 +18,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
  
 // Include config file
-require_once "config.php";
+require_once "db/config.php";
  
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -74,15 +74,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             header("location: welcome.php");
                         } else{
                             // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
+                            $password_err = "Felaktigt lösenord";
                         }
                     }
                 } else{
                     // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
+                    $username_err = "Finns ingen användare med det namnet.";
                 }
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "Något gick fel, försök gärna igen.";
             }
 
             // Close statement
@@ -96,7 +96,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 
-
+        <div class="container">
+        <div class="row">
+        <div class="m-auto">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <h2>Login</h2>
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="create-user.php">Sign up now</a>.</p>
+        </form>
+        </div>
+        </div>
+        </div>
+    </div>
 
 
 

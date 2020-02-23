de39ce1676e0218aee1ca07e309dfd16d31bd2a7
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-02-23 17:39:31 +0100

    changed some in config

--- templates/get-events.php
+++ templates/get-events.php
@@ -1,18 +1,8 @@
 <? require('db/config.php');
 
+//Pointing to what data to fetch/get
 $sql = "SELECT * FROM events";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-//Pointing to what data to fetch/get
-/*$sql = "SELECT * FROM events";
-
-$events = $conn->query($sql);
-
-$conn->close();
-
 ?>
-*/

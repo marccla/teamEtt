a0e4381440eb24c2b345b7b06aa75d0cb714ed92
sandra <s.pedersen.lundell@gmail.com>
2020-02-24 18:23:45 +0100

    Import nav bar template

--- welcome.php
+++ welcome.php
@@ -9,6 +9,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 ?>
 <? include('templates/head.php'); ?>
+<? include('templates/nav.php'); ?>
 <body>
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>

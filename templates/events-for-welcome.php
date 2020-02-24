a8f1f13462340aa47529f2fc9da71a55b4a12af9
Marc <hello@marclarsson.se>
2020-02-24 13:36:34 +0100

    bugfix

--- templates/events-for-welcome.php
+++ templates/events-for-welcome.php
@@ -7,13 +7,13 @@
     foreach ($events as $event) {
      ?> 
  <div class="col-lg-3 col-6 col-12">
-    <div class="card" style="width: 18rem;">
+    <div class="card">
   <img src="<? echo $event['img_url'] ?>" class="card-img-top" alt="picture">
   <div class="card-body">
     <h5 class="card-title" style="color: #3D4849;"><? echo $event['title'] ?></h5>
     <p class="card-text"><? echo $event['text'] ?></p>
     <a href="#" class="btn btn-primary" style="background-color: #B3997F;">Edit</a>
-    <a href="#" class="btn btn-secondary" style="width: 30% !important; height: 30% !imporant; background-color: #6DA0B3;">Delete</a>
+    <a href="#" class="btn btn-secondary" style="width: 30% !important; height: 30% !important; background-color: #6DA0B3;">Delete</a>
   </div>
  </div>
 </div>

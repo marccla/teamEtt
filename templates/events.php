b78367ed682cd7f6887f65071003f24f606fabf0
Marc <hello@marclarsson.se>
2020-02-24 12:24:44 +0100

    fixed styling on events

--- templates/events.php
+++ templates/events.php
@@ -6,14 +6,14 @@
 //Looping each row in table as its own "article"
     foreach ($events as $event) {
      ?> 
- <div class="col-lg-3 col-6 col-12">
+ <div class="col-lg-3 col-md-6 col-12">
     <div class="card" style="width: 18rem;">
   <img src="<? echo $event['img_url'] ?>" class="card-img-top" alt="picture">
   <div class="card-body">
     <h5 class="card-title" style="color: #3D4849;"><? echo $event['title'] ?></h5>
     <p class="card-text"><? echo $event['text'] ?></p>
     <a href="#" class="btn btn-primary" style="background-color: #B3997F;">Edit</a>
-    <a href="#" class="btn btn-secondary" style="width: 30% !important; height: 30% !imporant; background-color: #6DA0B3;">Delete</a>
+    <a href="#" class="btn btn-secondary" style="background-color: #6DA0B3;">Delete</a>
   </div>
  </div>
 </div>

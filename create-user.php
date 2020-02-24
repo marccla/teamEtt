917d65f8cf407e2f295458542588d90624d72467
Mirza <mirza.hajdarevic@medieinstitutet.se>
2020-02-24 10:31:54 +0100

    Fixed create-user form.

--- create-user.php
+++ create-user.php
@@ -101,7 +101,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
 
+<div class="container">
+<div class="row">
+<div class="wrapper">
+        <h2>Registrera konto</h2>
+        
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Användarnamn</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Lösenord</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Bekräfta lösenord</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Har du redan ett konto? <a href="login.php">Logga in här!</a>.</p>
+        </form>
+    </div>    
 
+</div>
+</div>
 
 
 

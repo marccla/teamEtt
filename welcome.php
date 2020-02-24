643960c4a6a5c64fe53c09fcd78cdda9c328d1b4
sandra <s.pedersen.lundell@gmail.com>
2020-02-24 18:31:46 +0100

    Comment out header stuff

--- welcome.php
+++ welcome.php
@@ -10,11 +10,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 ?>
 <? include('templates/head.php'); ?>
 <? include('templates/nav.php'); ?>
+<? /*  TODO: move to header?
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
     <p>
         <a href="log-out.php" class="btn">Sign Out of Your Account</a>
     </p>
+*/?>
 
     <? include('templates/events-for-welcome.php'); ?>

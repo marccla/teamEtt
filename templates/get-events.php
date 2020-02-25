38bb54af39a23c669cf668127c141c594808ee50
sandra <s.pedersen.lundell@gmail.com>
2020-02-25 23:26:10 +0100

    Fix sql query to include imgurl

--- templates/get-events.php
+++ templates/get-events.php
@@ -1,7 +1,7 @@
 <? require('db/config.php');
 
 //Pointing to what data to fetch/get
-$sql = "SELECT events.id, events.title, events.text, categories.name FROM events JOIN categories on events.category_id = categories.id";
+$sql = "SELECT events.id, events.title, events.text, events.img_url, categories.name FROM events JOIN categories on events.category_id = categories.id";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);

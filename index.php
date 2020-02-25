8b0fbec4e662c9ed7145eda6fb77cf98b4718798
sandra <s.pedersen.lundell@gmail.com>
2020-02-25 19:44:39 +0100

    Include create-event-modal

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include('templates/nav.php');
     include('db/config.php');
     include('templates/head.php');
-    
+    include('components/create-event.php');
     include('templates/events.php');
     include('templates/footer.php');
 

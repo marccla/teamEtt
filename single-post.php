b5c3316cae44fe8211ab8d82c3200ed1c7052bd5
marccla <marcclarsson@gmail.com>
2020-02-25 12:35:33 +0100

    added unique ids for all cards on index.php

--- single-post.php
+++ single-post.php
@@ -3,6 +3,29 @@
     include('templates/head.php');
     include('templates/nav.php'); ?>
 
+    <div class="container">
+    <div class="row">
+    <div class="col-12 col-lg-12 col-md-12">
+    <img class="event-img" src="#"> 
+    </div>
+    <div class="col-12 col-lg-6 col-md-6">
+    <form action="post">
+    <h2>Kommer du på eventet?</h2>
+    <label for="name">Namn</label>
+    <input type="text" name="sname" placeholder="Ditt namn" required>
+    </input><label for="email">Epost</label>
+    <input type="text" name="semail" placeholder="Epost" required></input>
+    </form>
+    </div>
+    <div class="col-12 col-md-6 col-lg6">
+    <h2>EVENT TITLE</h2>
+    <p>EVENT TEXT</p>
+    
+    </div>
+    
+    </div>
+    </div>
+
 
 
 

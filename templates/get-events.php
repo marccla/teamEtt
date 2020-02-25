6246c05e36cae923f9b90bc6ff15afdcb30788d5
sandra <s.pedersen.lundell@gmail.com>
2020-02-25 22:17:09 +0100

    Join events table with categories

--- templates/get-events.php
+++ templates/get-events.php
@@ -1,7 +1,7 @@
 <? require('db/config.php');
 
 //Pointing to what data to fetch/get
-$sql = "SELECT * FROM events";
+$sql = "SELECT * FROM events JOIN categories on events.category_id = categories.id";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);

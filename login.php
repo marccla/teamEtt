82c7738b538da695170c6ff1b1d366a4c5f9a603
marccla <marcclarsson@gmail.com>
2020-02-25 15:57:43 +0100

    fixed english to swedish

--- login.php
+++ login.php
@@ -100,21 +100,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <div class="row">
         <div class="m-auto">
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <h2>Login</h2>
+        <h2>Logga in</h2>
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
+                <label>Användarnamn</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
+                <label>Lösenord</label>
                 <input type="password" name="password" class="form-control">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="create-user.php">Sign up now</a>.</p>
+            <p>Har du inget konto?<a href="create-user.php">Skapa ett konto!</a>.</p>
         </form>
         </div>
         </div>

33ba380dfb59c9524b3a7c044682148820f34596
sandra <s.pedersen.lundell@gmail.com>
2020-02-26 10:01:40 +0100

    Conditional render of img

--- templates/events.php
+++ templates/events.php
@@ -12,27 +12,25 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     <?
     //Looping each row in table as its own "article"
     foreach ($events as $event) { ?>
-      <a href="single-post.php?id=<?php echo $event['id'];?>">
+      <a href="single-post.php?id=<?php echo $event['id']; ?>">
         <div class="col-lg-3 col-md-6 col-12">
           <div class="card" style="width: 18rem;">
-           <img src="<? echo $event['img_url'] ?>" class="card-img-top" alt="picture">
-           <div class="card-body">
-             <h5 class="card-title" style="color: #3D4849;"><? echo $event['title'] ?></h5><span class="badge badge-primary"><? echo $event['name'] ?></span>
-             <p class="card-text"><? echo $event['text'] ?></p>
-      
-             <?php if ($loggedIn) : ?>
-               <a href="#" class="btn btn-primary" style="background-color: #B3997F;">Edit</a>
-               <a href="delete-event.php?id=<?php echo $event['id'] ?>" class="btn btn-secondary" style="width: 30% !important; height: 30% !imporant; background-color: #6DA0B3;">Delete</a>
-             <?php endif; ?>
-             
+            <?php if ($event['img_url']) : ?>
+              <img src="<? echo $event['img_url'] ?>" class="card-img-top" alt="picture">
+            <?php endif; ?>
+            <div class="card-body">
+              <h5 class="card-title" style="color: #3D4849;"><? echo $event['title'] ?></h5><span class="badge badge-primary"><? echo $event['name'] ?></span>
+              <p class="card-text"><? echo $event['text'] ?></p>
+
+              <?php if ($loggedIn) : ?>
+                <a href="#" class="btn btn-primary" style="background-color: #B3997F;">Edit</a>
+                <a href="delete-event.php?id=<?php echo $event['id'] ?>" class="btn btn-secondary" style="width: 30% !important; height: 30% !imporant; background-color: #6DA0B3;">Delete</a>
+              <?php endif; ?>
+
+            </div>
           </div>
         </div>
-      </div>
       </a>
     <?  } ?>
   </div>
- </div>
-
-
-
- 
+</div>
